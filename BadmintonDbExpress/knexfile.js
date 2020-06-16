29335b3e13f1e0e55918cc6acc9f01eddd75c94d
Tony Wang <tonywang0697@yahoo.com>
2020-06-15 23:03:20 -0400

    readded dev

--- BadmintonDbExpress/knexfile.js
+++ BadmintonDbExpress/knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
   //   connection: `${process.env.DEV_DATABASE_URL}/${process.env.DEV_DATABASE_NAME}`
   // },
 
-  prod: {
+  development: {
     client: 'pg',
     connection: `${process.env.PROD_DATABASE_URL}/${process.env.PROD_DATABASE_NAME}`
   },
