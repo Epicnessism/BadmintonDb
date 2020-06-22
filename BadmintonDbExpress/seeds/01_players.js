2ee4074badf3bb4b7a269ed5e5286dfd432c8cf7
Tony Wang <tonywang0697@yahoo.com>
2020-06-21 20:12:36 -0400

    forgot birthday null constraint

--- BadmintonDbExpress/seeds/01_players.js
+++ BadmintonDbExpress/seeds/01_players.js
@@ -9,7 +9,7 @@ exports.seed = function(knex) {
         {player_id: "101e4567-e89b-12d3-a456-426614174111", given_name: 'Gus', family_name: 'Walzer', birthday: '01/01/1987'},
         {player_id: "143e4567-e89b-12d3-a456-426614174033", given_name: 'Zoe', family_name: 'Xu', birthday: '06/20/1996', level: 'B', hand: 'right', location: 'VA', institution: 'GMU'},
         {player_id: "144e4567-e89b-12d3-a456-426614174001", given_name: 'Pralfah', family_name: 'SomethingLong', birthday: '06/20/1996', level: 'A', hand: 'right', location: 'VA', institution: 'GMU'},
-        {player_id: "155e4567-e89b-12d3-a456-426614174009", given_name: 'Kyle', family_name: 'Lin', level: 'A', hand: 'right', location: 'VA'},
+        {player_id: "155e4567-e89b-12d3-a456-426614174009", given_name: 'Kyle', family_name: 'Lin', birthday: '01/20/1994', level: 'A', hand: 'right', location: 'VA'},
       ]);
     });
 };
