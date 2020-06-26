1f771398f3a43a306712bbe2b3cc4f2851ca7f52
Tony Wang <tonywang0697@yahoo.com>
2020-06-26 02:52:20 -0400

    forgot to change points to points_1 and points_2:

--- BadmintonDbExpress/seeds/05_games.js
+++ BadmintonDbExpress/seeds/05_games.js
@@ -5,14 +5,14 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('games').insert([
-        {player_id_1A: 1, player_id_1B: 2, player_id_2A: 3, player_id_2B: 4, set_id: 1, points_A: 21, points_B: 17},
-        {player_id_1A: 1, player_id_1B: 2, player_id_2A: 3, player_id_2B: 4, set_id: 1, points_A: 11, points_B: 21},
-        {player_id_1A: 1, player_id_1B: 2, player_id_2A: 3, player_id_2B: 4, set_id: 1, points_A: 23, points_B: 21},
-        {player_id_1A: 2, player_id_2A: 3, set_id: 2, tournament_id: 1, points_A: 13, points_B: 21},
-        {player_id_1A: 1, player_id_2A: 4, set_id: 3, points_A: 1, points_B: 21},
-        {player_id_1A: 1, player_id_1B: 2, player_id_2A: 3, player_id_2B: 4, set_id: 4, points_A: 29, points_B: 27},
-        {player_id_1A: 2, player_id_2A: 3, set_id: 2, tournament_id: 1, points_A: 19, points_B: 21},
-        {player_id_1A: 1, player_id_1B: 2, player_id_2A: 3, player_id_2B: 4, set_id: 4, points_A: 11, points_B: 21},
+        {player_id_1A: 1, player_id_1B: 2, player_id_2A: 3, player_id_2B: 4, set_id: 1, points_1: 21, points_2: 17},
+        {player_id_1A: 1, player_id_1B: 2, player_id_2A: 3, player_id_2B: 4, set_id: 1, points_1: 11, points_2: 21},
+        {player_id_1A: 1, player_id_1B: 2, player_id_2A: 3, player_id_2B: 4, set_id: 1, points_1: 23, points_2: 21},
+        {player_id_1A: 2, player_id_2A: 3, set_id: 2, tournament_id: 1, points_1: 13, points_2: 21},
+        {player_id_1A: 1, player_id_2A: 4, set_id: 3, points_1: 1, points_2: 21},
+        {player_id_1A: 1, player_id_1B: 2, player_id_2A: 3, player_id_2B: 4, set_id: 4, points_1: 29, points_2: 27},
+        {player_id_1A: 2, player_id_2A: 3, set_id: 2, tournament_id: 1, points_1: 19, points_2: 21},
+        {player_id_1A: 1, player_id_1B: 2, player_id_2A: 3, player_id_2B: 4, set_id: 4, points_1: 11, points_2: 21},
       ]);
     });
 };
