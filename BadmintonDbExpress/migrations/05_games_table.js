95cc2276f6d931eec8e6f1469c3a206822cb23aa
Tony Wang <tonywang0697@yahoo.com>
2020-06-26 02:50:38 -0400

    fixed 1B/2A issue, its not team 1A/B and team 2A/B

--- BadmintonDbExpress/migrations/05_games_table.js
+++ BadmintonDbExpress/migrations/05_games_table.js
@@ -2,8 +2,8 @@ exports.up = function(knex) {
     return knex.schema.createTable('games', table => {
         table.increments('game_id');
         table.integer('player_id_1A').notNullable();
-        table.integer('player_id_2A').defaultTo(null);
-        table.integer('player_id_1B').notNullable();
+        table.integer('player_id_1B').defaultTo(null);
+        table.integer('player_id_2A').notNullable();
         table.integer('player_id_2B').defaultTo(null);
         table.integer('set_id').notNullable();
         table.integer('tournament_id').defaultTo(null);
@@ -15,8 +15,8 @@ exports.up = function(knex) {
 
         table.foreign('tournament_id').references('tournament_id').inTable('tournaments');
 
-        table.integer('points_A').notNullable();
-        table.integer('points_B').notNullable();
+        table.integer('points_1').notNullable();
+        table.integer('points_2').notNullable();
 
     })
   };
--- BadmintonDbExpress/routes/games.js
+++ BadmintonDbExpress/routes/games.js
@@ -23,34 +23,34 @@ games.get('/:id', function (req, res, next) {
 
 //insert a new game
 games.post('/', function (req, res, next) {
-    console.log(req.body);
-    console.log("2A:",req.body.player_2A);
-    console.log(req.body.player_2A == "");
-    
-    
-    if (req.body.new_set != "null") { //existing game
-        knex('games')
-            .insert({
-                player_id_1A: req.body.player_1A,
-                player_id_2A: req.body.player_2A != "" ? req.body.player_2A : null,
-                player_id_1B: req.body.player_1B,
-                player_id_2B: req.body.player_2B != "" ? req.body.player_2B : null,
-                set_id: req.body.set_id,
-                points_A: req.body.points_A,
-                points_B: req.body.points_B
-            })
-            .then(result => {
-                console.log(result);
-                if (result.length > 1) {
-                    res.status(400).json({ message: "more than 1 found..." })
-                } else {
-                    res.status(200).json(result[0]); //successfully inputted game record
-                }
-            })
+    if (req.body.gameType == "Singles") {
+        if (req.body.new_set != "null") { //existing game
+            knex('games')
+                .insert({
+                    player_id_1A: req.body.player_1A,
+                    player_id_1B: req.body.player_1B != "" ? req.body.player_1B : null,
+                    player_id_2A: req.body.player_2A,
+                    player_id_2B: req.body.player_2B != "" ? req.body.player_2B : null,
+                    set_id: req.body.set_id,
+                    points_A: req.body.points_A,
+                    points_B: req.body.points_B
+                })
+                .then(result => {
+                    console.log(result);
+                    if (result.length > 1) {
+                        res.status(400).json({ message: "more than 1 found..." })
+                    } else {
+                        res.status(200).json(result[0]); //successfully inputted game record
+                    }
+                })
 
-    } else { //new game, find new set_id
+        } else { //new game, find new set_id
 
+        }
+    } else {
+        
     }
+
     res.status(201).json({ 'post': req.params.id });
 })
 
